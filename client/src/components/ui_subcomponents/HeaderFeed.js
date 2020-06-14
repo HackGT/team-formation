04807458601da3c1dd0a92153ff422722ef62e0c
Rashmi Athavale <rathavale6@gatech.edu>
2020-06-13 23:03:07 -0700

    put code in headerfeed

--- client/src/components/ui_subcomponents/HeaderFeed.js
+++ client/src/components/ui_subcomponents/HeaderFeed.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../css/Headers.css';
 import { Button, Menu } from 'semantic-ui-react';
+import { Header, Image, Modal } from "semantic-ui-react";
 import { commitMutation } from 'react-relay';
 import {graphql} from 'babel-plugin-relay/macro';
 import environment from '../Environment';
@@ -20,6 +21,10 @@ mutation HeaderFeedMutation($uuid: String) {
 `
 
 class Headers extends Component {
+
+    state = {
+        open: false,
+    }
 
     render() {
         let toggle_text;
@@ -59,7 +64,21 @@ class Headers extends Component {
 				<div className="headers">
 					<h1>HackGT</h1>
 					<h2>Team Formation</h2>
-				</div>
+                </div>
+                <Modal isOpen={this.state.open}>
+                <Modal.Header>Select a Photo</Modal.Header>
+                <Modal.Content image>
+                    <Image wrapped size='medium' src='https://react.semantic-ui.com/images/avatar/large/rachel.png' />
+                    <Modal.Description>
+                    <Header>Default Profile Image</Header>
+                    <p>
+                        We've found the following gravatar image associated with your e-mail
+                        address.
+                    </p>
+                    <p>Is it okay to use this photo?</p>
+                    </Modal.Description>
+                </Modal.Content>
+                </Modal>
 			</div>
 		);
     };
