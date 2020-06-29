22fb74e7ac76910760f8a2db2559ca1eab0112b6
agupte31 <aakashagupte@gatech.edu>
2020-06-29 13:50:49 -0400

    Adding ability to switch between Team and User Pages with the press of a button

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import FeedTeamCards from './FeedTeamCards';
+import FeedCards from './FeedCards';
 import SideMenu from './SideMenu';
+import { Button } from 'semantic-ui-react';
+import { setState } from 'semantic-ui-react';
 import './css/Feed.css';
 
 class Feed extends Component {
@@ -8,28 +11,36 @@ class Feed extends Component {
     constructor(props){
         super(props)
         this.state = {
-            skill: ""
+			displayTeams: false,
+			individuals: true,
+			teams: false
 		};
-    };
+	};
+
 
     render() {
-		return (
-			<div className="Feed-container">
-				<div className="menu">
-					{/* <SideMenu onSearchClick={this.onSearchClick} className="search"/> */}
-				</div>
-			    <FeedTeamCards skill={this.state.skill} user_id={this.props.user_id} />
+		var cards = this.state.displayTeams ? <FeedTeamCards skill={this.state.skill} user_id={this.props.user_id} /> : <FeedCards skill={this.state.skill} user_id={this.props.user_id} />
+		return (<div className="Feed-container">
+			<Button.Group>
+				<Button id="user" onClick={() => this.setState({displayTeams: false, individuals: true, teams: false})} basic={!this.state.individuals} color='purple'>Individuals</Button>
+				<Button.Or />
+				<Button id="team" onClick={() => this.setState({displayTeams: true, individuals: false, teams: true})} basic={!this.state.teams} color='purple'>Team</Button>
+			</Button.Group>
+			{cards}
+			<div className="menu">
+						{/* <SideMenu onSearchClick={this.onSearchClick} className="search"/> */}
 			</div>
-
-        );
+		</div>);
 	};
+
 
 	onSearch = (search_string) => {
 		this.setState({skill:search_string});
 	};
 
     onSearchClick = (search_string) => {
-        this.setState({skill:search_string});
+		this.setState({skill:search_string});
+		
     };
 };
 
