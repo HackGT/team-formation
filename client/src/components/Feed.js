1a28dbb3bcc5233bbf9dbea002c7643e2eb13bff
agupte31 <aakashagupte@gatech.edu>
2020-06-29 20:23:37 -0400

    Button moved to the middle.

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import FeedTeamCards from './FeedTeamCards';
 import FeedCards from './FeedCards';
 import SideMenu from './SideMenu';
-import { Button } from 'semantic-ui-react';
+import { Button, Grid } from 'semantic-ui-react';
 import { setState } from 'semantic-ui-react';
 import './css/Feed.css';
 
@@ -21,11 +21,15 @@ class Feed extends Component {
     render() {
 		var cards = this.state.displayTeams ? <FeedTeamCards skill={this.state.skill} user_id={this.props.user_id} /> : <FeedCards skill={this.state.skill} user_id={this.props.user_id} />
 		return (<div className="Feed-container">
-			<Button.Group>
-				<Button id="user" onClick={() => this.setState({displayTeams: false, individuals: true, teams: false})} basic={!this.state.individuals} color='purple'>Individuals</Button>
-				<Button.Or />
-				<Button id="team" onClick={() => this.setState({displayTeams: true, individuals: false, teams: true})} basic={!this.state.teams} color='purple'>Team</Button>
-			</Button.Group>
+			<Grid>
+				<Grid.Column textAlign="center">
+					<Button.Group>
+						<Button id="user" onClick={() => this.setState({displayTeams: false, individuals: true, teams: false})} basic={!this.state.individuals} color='purple'>Individuals</Button>
+						<Button.Or />
+						<Button id="team" onClick={() => this.setState({displayTeams: true, individuals: false, teams: true})} basic={!this.state.teams} color='purple'>Team</Button>
+					</Button.Group>
+				</Grid.Column>
+			</Grid>
 			{cards}
 			<div className="menu">
 						{/* <SideMenu onSearchClick={this.onSearchClick} className="search"/> */}
