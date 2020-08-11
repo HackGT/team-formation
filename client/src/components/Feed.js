97317cd083021837a220facecc4e0d1af3bc731f
Rashmi Athavale <rathavale6@gatech.edu>
2020-08-10 19:00:41 -0700

    homepage css (#72)
    * moved css
    
    * deleted stuff
    
    Co-authored-by: Rashmi Athavale <rashmiathavale@Rashmis-MacBook-Pro.local>

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -10,80 +10,84 @@ import "./css/Feed.css";
 import TeamInformation from "./TeamInformation";
 
 class Feed extends Component {
-	state = {
-		searchTerm: "",
-		skills: [],
-		years: [],
-		schools: [],
-		teams: false
-	}
+  state = {
+    searchTerm: "",
+    skills: [],
+    years: [],
+    schools: [],
+    teams: false,
+  };
 
-    render() {
-		var cards = this.state.teams ? <FeedTeamCards search={this.state.searchTerm} onTeamPageClick={this.props.onTeamPageClick} skill={this.state.skills} user_id={this.props.user_id} /> :
-			<FeedCards search={this.state.searchTerm} skill={this.state.skills} grad_year={this.state.years} school={this.state.schools} user_id={this.props.user_id} />
-		return (
-			<div>
-                <p class="HackGTitle">HACKGT7: REIMAGINE REALITY</p>
-				<div className="switch-feed">
-                     <span class="teamFormation">HackGT Team Formation</span>
-					<Button.Group
-                        style={{
-                          background: "rgba(255, 251, 251, 0.7)",
-                          borderRadius: 15,
-                          padding: 3,
-                        }}
-                    >
-						<Button onClick={this.feedTypeListener} basic={this.state.teams}
-                        style={{
-                          backgroundColor: "#A8C5D6",
-                          color: "white",
-                          borderRadius: 15,
-                          fontFamily: "Quicksand-Bold",
-                          display: "flex",
-                          alignItems: "center",
-                          justifyContent: "center",
-                        }}
-                            >Individuals</Button>
-						<Button onClick={this.feedTypeListener} basic={!this.state.teams}
-                        style={{
-                          backgroundColor: "#A8C5D6",
-                          color: "white",
-                          borderRadius: 15,
-                          fontFamily: "Quicksand-Bold",
-                        }}
-                            >Teams</Button>
-					</Button.Group>
-				</div>
-				<div className="Feed-container">
-					<div className="menu">
-						<SideMenu className="Side-menu"
-							allFilterClickListener={this.allFilterClickListener}
-							onSearchClick={this.onSearchClick}
-							onTeamPage={this.state.teams}
-						/>
-					</div>
-						{this.state.skills.length || this.state.years.length || this.state.schools.length ?
-							<div className="user-input">
-								<div className="filters-applied">
-									<text>Filters Applied</text>
-								</div>
-								<div className='filter-tags'>
-									<InputTagCollection
-										skills={this.state.skills}
-										years={this.state.years}
-										schools={this.state.schools}
-										allFilterClickListener={this.allFilterClickListener}
-									/>
-								</div>
-							</div>
-						: null}
-						<div className="feed-cards">
-							{cards}
-						</div>
-				</div>
-			</div>
-        );
-	};
+  render() {
+    var cards = this.state.teams ? (
+      <FeedTeamCards
+        search={this.state.searchTerm}
+        onTeamPageClick={this.props.onTeamPageClick}
+        skill={this.state.skills}
+        user_id={this.props.user_id}
+      />
+    ) : (
+      <FeedCards
+        search={this.state.searchTerm}
+        skill={this.state.skills}
+        grad_year={this.state.years}
+        school={this.state.schools}
+        user_id={this.props.user_id}
+      />
+    );
+    return (
+      <div>
+        <p class="HackGTitle">HACKGT7: REIMAGINE REALITY</p>
+        <div className="switch-feed">
+          <span class="teamFormation">HackGT Team Formation</span>
+          <Button.Group>
+            <Button
+              className="individuals"
+              onClick={this.feedTypeListener}
+              basic={this.state.teams}
+            >
+              Individuals
+            </Button>
+            <Button
+              onClick={this.feedTypeListener}
+              basic={!this.state.teams}
+              className="teams"
+            >
+              Teams
+            </Button>
+          </Button.Group>
+        </div>
+        <div className="Feed-container">
+          <div className="menu">
+            <SideMenu
+              className="Side-menu"
+              allFilterClickListener={this.allFilterClickListener}
+              onSearchClick={this.onSearchClick}
+              onTeamPage={this.state.teams}
+            />
+          </div>
+          {this.state.skills.length ||
+          this.state.years.length ||
+          this.state.schools.length ? (
+            <div className="user-input">
+              <div className="filters-applied">
+                <text>Filters Applied</text>
+              </div>
+              <div className="filter-tags">
+                <InputTagCollection
+                  skills={this.state.skills}
+                  years={this.state.years}
+                  schools={this.state.schools}
+                  allFilterClickListener={this.allFilterClickListener}
+                />
+              </div>
+            </div>
+          ) : null}
+          <div className="feed-cards">{cards}</div>
+        </div>
+      </div>
+    );
+  }
 
   allFilterClickListener = (name, filterProp) => {
     let index = this.state[filterProp].indexOf(name);
@@ -103,30 +107,29 @@ class Feed extends Component {
     this.setState({ searchTerm: e.target.value });
   };
 
-    onSearchClick = (search_string) => {
-        this.setState({searchTerm:search_string});
-	};
+  onSearchClick = (search_string) => {
+    this.setState({ searchTerm: search_string });
+  };
 
-	feedTypeListener = (e, data) => {
-		if (data.children === "Individuals") {
-			this.setState({
-				teams: false,
-				searchTerm: "",
-				skills: [],
-				years: [],
-				schools: []
-			});
-		}
-		else if (data.children === "Teams") {
-			this.setState({
-				teams: true,
-				searchTerm: "",
-				skills: [],
-				years: [],
-				schools: []
-			});
-		}
-	};
-};
+  feedTypeListener = (e, data) => {
+    if (data.children === "Individuals") {
+      this.setState({
+        teams: false,
+        searchTerm: "",
+        skills: [],
+        years: [],
+        schools: [],
+      });
+    } else if (data.children === "Teams") {
+      this.setState({
+        teams: true,
+        searchTerm: "",
+        skills: [],
+        years: [],
+        schools: [],
+      });
+    }
+  };
+}
 
 export default Feed;
--- client/src/components/ui_subcomponents/TeamRequest.js
+++ client/src/components/ui_subcomponents/TeamRequest.js
@@ -4,88 +4,59 @@ import "../css/Modal.css";
 
 class TeamRequest extends Component {
   render() {
-    const sender = this.props.sender
+    const sender = this.props.sender;
     return (
       <Modal
-        style={{
-          padding: 10,
-          background:
-            "linear-gradient(180deg, #656CAE 0%, rgba(255, 255, 255, 0) 100%), #8BB2C2",
-        }}
         closeIcon
         open={this.props.showModal}
         onClose={() => {
           this.props.closeModal();
         }}
       >
-        <Modal.Content
-          style={{
-            background:
-              "linear-gradient(180deg, #656CAE 0%, rgba(255, 255, 255, 0) 100%), #8BB2C2",
-          }}
-        >
+        <Modal.Content>
           <Modal.Description>
-            <div class="background">
-              <p class="modalHeader">
-                {this.props.teamName} Wants You to Join Their Team!
-              </p>
-              <div class="modal1Margins">
-                <p class="requestMessage">
-                  {this.props.teamName}'s Request Message:
-                </p>
-                <p class="requestMessageBody">
-                  {this.props.teamRequestMessage}
-                </p>
-                <p class="projectIdea">{this.props.teamName}'s Project Idea:</p>
-                <p class="projectIdeaBody">{this.props.teamProjectIdea}</p>
+            <p class="modalHeader">
+              {sender.name} Wants You to Join Their Team!
+            </p>
+            <div class="modal1Margins">
+              <p class="requestMessage">{sender.name}'s Request Message:</p>
+              <p class="requestMessageBody">{this.props.teamRequestMessage}</p>
+              <p class="projectIdea">{sender.name}'s Project Idea:</p>
+              <p class="projectIdeaBody">{this.props.teamProjectIdea}</p>
 
-                <div class="flex-container1">
-                  <div>
-                    <Button
-                      style={{
-                        borderRadius: 12,
-                        color: "white",
-                        background: "rgba(255, 255, 255, 0.22)",
-                        fontFamily: "Quicksand-Bold",
-                        padding: 12,
-                      }}
-                      onClick={this.onViewTeamClick}
-                    >
-                      View more about {sender.name}
-                    </Button>
-                  </div>
+              <div class="flex-container1">
+                <div>
+                  <Button
+                    className="submit"
+                    style={{
+                      padding: 12,
+                    }}
+                    onClick={this.onViewTeamClick}
+                  >
+                    View more about {sender.name}
+                  </Button>
                 </div>
-                <div class="flex-container2">
-                  <div class="buttonMargin">
-                    <Button
-                      style={{
-                        borderRadius: 12,
-                        color: "white",
-                        background: "rgba(255, 255, 255, 0.22)",
-                        fontFamily: "Quicksand-Bold",
-                      }}
-                      onClick={() => {
-                        this.props.closeModal();
-                      }}
-                    >
-                      Accept
-                    </Button>
-                  </div>
-                  <div class="buttonMargin">
-                    <Button
-                      style={{
-                        borderRadius: 12,
-                        color: "white",
-                        background: "rgba(255, 255, 255, 0.22)",
-                        fontFamily: "Quicksand-Bold",
-                      }}
-                      onClick={() => {
-                        this.props.closeModal();
-                      }}
-                    >
-                      Deny
-                    </Button>
-                  </div>
+              </div>
+              <div class="flex-container2">
+                <div class="buttonMargin">
+                  <Button
+                    className="submit"
+                    onClick={() => {
+                      this.props.closeModal();
+                    }}
+                  >
+                    Accept
+                  </Button>
+                </div>
+                <div class="buttonMargin">
+                  <Button
+                    className="submit"
+                    onClick={() => {
+                      this.props.closeModal();
+                    }}
+                  >
+                    Deny
+                  </Button>
                 </div>
               </div>
             </div>
