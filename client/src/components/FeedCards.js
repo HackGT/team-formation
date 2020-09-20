1706f8e2f0a72bf02e4866cae410303cf9bb84d1
Rahul Rajan <rahul.rajanus@gmail.com>
2020-09-20 18:12:26 -0400

    change request message + contact

--- client/src/components/FeedCards.js
+++ client/src/components/FeedCards.js
@@ -12,6 +12,7 @@ const getUsersQuery = graphql `
     query FeedCardsQuery($skill: String, $grad_year: String, $school: String, $search: String) {
         users(skill:$skill, grad_year:$grad_year, school:$school, search:$search) {
             name
+            email
             school
             grad_year
             contact
@@ -52,7 +53,7 @@ class FeedCards extends Component {
                     // console.log("HELLO" + props.user_profile.team.id);
                     let cards = props.users.map(user => {
                         console.log("Stuff: " + user.id);
-                        return <UserCard className='card-individual' name={user.name} grad_year={user.grad_year} school={user.school} contact={user.contact} skills={user.skills.filter(function(el) {
+                        return <UserCard className='card-individual' name={user.name} grad_year={user.grad_year} school={user.school} contact={user.email} skills={user.skills.filter(function(el) {
                                 return Boolean(el);
                             })} experience={user.experience} id={user.id} team={props.user_profile.team}/>
                     })
