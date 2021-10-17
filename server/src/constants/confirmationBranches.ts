d84dddddf0c19c35cf4c7d60328dc867eaa5cfe3
Rahul Rajan <rahul.rajanus@gmail.com>
2021-10-17 17:17:33 -0400

    Update confirmationBranches.ts

--- server/src/constants/confirmationBranches.ts
+++ server/src/constants/confirmationBranches.ts
@@ -1,6 +1,6 @@
 export enum confirmationBranches {
-  EMERGING_INPERSON = "Participant-Emerging In-Person",
-  EMERGING_VIRTUAL = "Participant-Emerging Virtual",
-  GENERAL_INPERSON = "Participant-General In-Person",
-  GENERAL_VIRTUAL = "Participant-General Virtual",
+  EMERGING_INPERSON = "Participant-Emerging In-Person Confirmation",
+  EMERGING_VIRTUAL = "Participant-Emerging Virtual Confirmation",
+  GENERAL_INPERSON = "Participant-General In-Person Confirmation",
+  GENERAL_VIRTUAL = "Participant-General Virtual Confirmation",
 }
